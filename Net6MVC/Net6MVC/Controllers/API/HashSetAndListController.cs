8255a54b815513aa2a1188d5cc86e6dd04302ee1
matol <omatolo@gmail.com>
2022-10-16 21:06:32 +0800

    測試頁面 - HashSet 與 List效能比較

---
+++ Net6MVC/Net6MVC/Controllers/API/HashSetAndListController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Net6MVC.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HashSetAndListController : ControllerBase
+    {
+        // GET: api/<HashSetAndListController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            Stopwatch sw = new Stopwatch();
+            HashSet<TestModel> hs = new HashSet<TestModel>();
+            List<TestModel> list = new List<TestModel>();
+            HashSet<string> hs2 = new HashSet<string>();
+            List<string> list2 = new List<string>();
+
+            // 999,999
+            for (int i = 0; i < 999999; i++)
+            {
+                var model = new TestModel
+                {
+                    index = i,
+                    name = "name-" + i.ToString(),
+                };
+                hs.Add(model);
+                list.Add(model);
+
+                hs2.Add("string-" + i.ToString());
+                list2.Add("string-" + i.ToString());
+            }
+
+            sw.Reset();
+            sw.Start();
+            if (hs.Select(s => s.name).Contains("name-45678"))
+            {
+                var t = 1;
+            }
+
+            sw.Stop();
+
+            string result1 = sw.Elapsed.TotalMilliseconds.ToString();
+
+            sw.Reset();
+            sw.Start();
+            if (list.Select(s => s.name).Contains("name-45678"))
+            {
+                var t = 1;
+            }
+            sw.Stop();
+
+            string result2 = sw.Elapsed.TotalMilliseconds.ToString();
+
+            sw.Reset();
+            sw.Start();
+            if (hs2.Contains("string-67784"))
+            {
+                var t = 1;
+            }
+            sw.Stop();
+
+            string result3 = sw.Elapsed.TotalMilliseconds.ToString();
+
+            sw.Reset();
+            sw.Start();
+            if (list2.Contains("string-67784"))
+            {
+                var t = 1;
+            }
+            sw.Stop();
+
+            string result4 = sw.Elapsed.TotalMilliseconds.ToString();
+
+            return new string[] { result1, result2, result3, result4 };
+        }
+
+        public class TestModel
+        {
+            public int index { get; set; }
+            public string name { get; set; }
+        }
+    }
+}
---
+++ Net6MVC/Net6MVC/Controllers/CollectionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Net6MVC.Controllers
+{
+    public class CollectionController : Controller
+    {
+        /// <summary>
+        /// 測試兩者效率
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult HashSetAndList()
+        {
+            return View();
+        }
+
+
+    }
+}
